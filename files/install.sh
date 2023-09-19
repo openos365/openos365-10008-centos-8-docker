b44bf42bd8728b3ae64755635990b5d01ed03539
gnuhub <gnuhub@gmail.com>
2023-09-19 18:16:44 +0800

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -29,11 +29,11 @@ dnf makecache
 dnf --assumeyes install epel-release dnf-plugins-core
 dnf --assumeyes upgrade epel-release
 
-sed -e 's!^metalink=!#metalink=!g' \
-    -e 's!^#baseurl=!baseurl=!g' \
-    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
-    -e 's!https\?://download\.example/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
-    -i /etc/yum.repos.d/epel*.repo
+# sed -e 's!^metalink=!#metalink=!g' \
+#     -e 's!^#baseurl=!baseurl=!g' \
+#     -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
+#     -e 's!https\?://download\.example/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
+#     -i /etc/yum.repos.d/epel*.repo
 
 crb enable
 
