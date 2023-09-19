e70d1ba101b2f50fce95719525d6d341b5469367
gnuhub <gnuhub@gmail.com>
2023-09-19 17:01:19 +0800

    (just fix it)

--- 2.docker.run.install.sh
+++ 2.docker.run.install.sh
@@ -6,6 +6,6 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 echo $PROJECT_NAME
 cd $CMD_PATH
 
-docker pull openanolis/anolisos:7.9-x86_64
+docker pull openos365/openos365-10004-openeuler-20-03-lts-docker-main:latest
 
-docker run -i -v ./:/code -w /code openanolis/anolisos:7.9-x86_64 /code/files/install.sh
+docker run -i -v ./:/code -w /code openos365/openos365-10004-openeuler-20-03-lts-docker-main:latest /code/files/install.sh
