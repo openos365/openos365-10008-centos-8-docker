9f8aa47eac5bc48b73b8db2a324a35362f7af633
gnuhub <gnuhub@gmail.com>
2023-09-19 14:13:01 +0800

    (just fix it)

---
+++ debug.run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+
+docker pull openanolis/anolisos:8
+
+docker run -it -v ./:/code -w /code openanolis/anolisos:8 /code/files/install.sh
