38aa95845f892e867df5c0b5c3ec326c456c752d
gnuhub <gnuhub@gmail.com>
2023-09-19 13:15:23 +0800

    (just fix it)

---
+++ ci.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -x
+
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+
+git config --global user.email "gnuhub@gmail.com"
+git config --global user.name "gnuhub"
+
+git remote -v
+git remote set-url origin git@github.com:${GITHUB_REPOSITORY}.git
+git remote -v
+
+ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "frs.sourceforge.net"
+ssh-keyscan "frs.sourceforge.net" >> /home/runner/.ssh/known_hosts
+ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "github.com"
+ssh-keyscan "github.com" >> /home/runner/.ssh/known_hosts
+cat /home/runner/.ssh/known_hosts
+
+
+env
+
+cd $CMD_PATH
+
+docker build . -f Dockerfile \
+-t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t gnuhub/$PROJECT_NAME-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t gnuhub/$PROJECT_NAME-$GITHUB_REF_NAME:latest \
+-t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+
+
+docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER 
+# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest 
+docker push gnuhub/$PROJECT_NAME-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+docker push gnuhub/$PROJECT_NAME-$GITHUB_REF_NAME:latest
+cd $CMD_PATH
+
+cid=$(docker run -it --detach ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER)
+
+git pull origin HEAD
+rm -rf versions
+rm -rf yum.repos.d
+docker cp ${cid}:/root/versions/ ./versions/
+docker cp ${cid}:/etc/yum.repos.d/ ./yum.repos.d/
+cd $CMD_PATH
+git add .
+git commit -a -m "CI-BOT:$(date +%Y.%m.%d-%H%M%S)-$GITHUB_REF_NAME-$GITHUB_RUN_NUMBER"
+git push origin HEAD
---
+++ files/install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -x
+
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+export TERM=xterm-256color
+echo $PROJECT_NAME
+cd $CMD_PATH
+env
+
+whoami
+pwd
+
+yum update -y
+yum install epel-release -y
+yum install dnf -y
+dnf --assumeyes update
+
+dnf --assumeyes install perl 
+dnf --assumeyes install rsync
+
+rsync -avzP ./root/ /
+
+
+dnf clean all
+dnf makecache
+
+dnf --assumeyes install epel-release dnf-plugins-core
+dnf --assumeyes upgrade epel-release
+
+sed -e 's!^metalink=!#metalink=!g' \
+    -e 's!^#baseurl=!baseurl=!g' \
+    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
+    -e 's!https\?://download\.example/pub/epel!https://mirrors.tuna.tsinghua.edu.cn/epel!g' \
+    -i /etc/yum.repos.d/epel*.repo
+
+crb enable
+
+dnf --assumeyes install kiwi 
+dnf --assumeyes install sudo
+dnf --assumeyes update
+dnf --assumeyes install qemu-img
+dnf --assumeyes install dosfstools
+
+cd ~
+if [ -d versions ];then
+    rm -rf versions
+fi
+mkdir versions
+cd versions
+dnf list installed > dnf.list.installed.txt
+dnf list > dnf.list.txt
+
+sed -i '1,2d' dnf.list.installed.txt
+sed -i '1d' dnf.list.txt
+
+
